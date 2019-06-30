d080fa80010845dbd3c2e4445cdbd5253ccdb2cb
nalleyer <nalleyer@gmail.com>
2019-06-30 17:10:39 +0800

    add config & d.sh

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports  = {
+  base: '/IMCCWIKI/',
+  title: 'IMCC WIKI',
+  description: 'imcc玩家的百科全书',
+  themeConfig: {
+    sidebar: [
+      '/',
+    ],
+    nav: [
+      {text: '首页', link: '/'},
+    ],
+  }
+}
